78da803c92e03cd318f4d7050d48547128960e62
X-Coder 72 <abhaysahu1636@gmail.com>
2026-04-03 23:26:25 +0530

    modify

--- backend/simulation/fallSimulation.js
+++ backend/simulation/fallSimulation.js
@@ -1,7 +1,18 @@
-const getFallData = () => ({
-    status: 'FALL_ALERT',
-    'vitals.heartRate': Math.floor(Math.random() * (140 - 120 + 1)) + 120, // High HR
-    'vitals.spo2': 94,
-    'location.locationName': 'Floor - Bedroom Area'
-});
-module.exports = getFallData;
+const User = require('../models/User');
+
+const triggerFall = async (phone) => {
+    return await User.findOneAndUpdate(
+        { phone },
+        {
+            $set: {
+                status: 'FALL_ALERT',
+                'vitals.heartRate': 132, // Rapid heart rate post-fall
+                'vitals.spo2': 94,
+                'location.locationName': 'Floor - Bedroom'
+            }
+        },
+        { returnDocument: 'after' }
+    );
+};
+
+module.exports = triggerFall;
